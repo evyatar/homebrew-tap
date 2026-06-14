36ea9300bd9f726b102c4c594fdc35ea890ca169
Evyatar Shalom <evyatar@siteam.io>
2026-06-14 20:31:50 +0300

    chore: postinstall selfsign

--- Casks/quick-json-viewer.rb
+++ Casks/quick-json-viewer.rb
@@ -1,6 +1,6 @@
 cask "quick-json-viewer" do
   version "1.0.0"
-  sha256 "705c29af4b6cf68029877966e0247676ac2ab85c51991a2303cb5dff840ab35d"
+  sha256 "19440bac9340dd87faf5698ab35276d12d10ddc8a102f0470b3a934aeead6653"
 
   url "https://github.com/evyatar/quick-json-viewer/releases/download/v#{version}/quick-json-viewer-v#{version}.zip"
   name "Quick JSON Viewer"
@@ -9,6 +9,18 @@ cask "quick-json-viewer" do
 
   app "quick-json-viewer.app"
 
+  postflight do
+    # 1. Strip the quarantine attribute
+    system_command "/usr/bin/xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/quick-json-viewer.app"],
+                   sudo: false
+
+    # 2. Force an Ad-Hoc code-signature
+    system_command "/usr/bin/codesign",
+                   args: ["--force", "--deep", "--sign", "-", "#{appdir}/quick-json-viewer.app"],
+                   sudo: false
+  end
+  
   zap trash: [
     "~/Library/Application Support/com.evyatar.quick-json-viewer",
     "~/Library/Preferences/com.evyatar.quick-json-viewer.plist",
